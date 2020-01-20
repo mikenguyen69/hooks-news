d10aab569d2e76344c2321391d9bc068bb6aba50
Mike Nguyen <Mike.Nguyen@nib.co.nz>
2020-01-21 10:11:17 +1300

    Firebase: refactor and complete the setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,3 +4,28 @@ import 'firebase/auth';
 
 
 app.initializeApp(firebaseConfig);
+
+class Firebase {
+    constructor() {
+        app.initializeApp(firebaseConfig);
+        this.auth = app.auth();
+    }
+
+    async register(name, email, password) {
+        const newUser = await this.auth.createUserWithEmailAndPassword(
+            email, 
+            password
+        );
+
+        return await newUser.user.updateProfile({
+            displayName: name
+        })
+    }
+
+    async login(email, password) {
+        return await this.auth.signInWithEmailAndPassword(email, password);
+    }
+}
+
+const firebase = new Firebase();
+export default firebase;
