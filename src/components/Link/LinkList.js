582ccdf09a66cef89e742eed75c57afabbcc5c19
Mike Nguyen <Mike.Nguyen@nib.co.nz>
2020-01-22 11:29:39 +1300

    Add serverless functions to return the list if the cursor is failed to present

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,11 +2,13 @@ import React from "react";
 import FirebaseContext from '../../firebase/context';
 import LinkItem from './LinkItem';
 import {LINKS_PER_PAGE} from '../../utils';
+import axios from 'axios';
 
 function LinkList(props) {
   const {firebase} = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
   const [cursor, setCursor] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
   const isTopPage = props.location.pathname.includes('top');
   const isNewPage = props.location.pathname.includes('new');
   const page = Number(props.match.params.page);
@@ -16,7 +18,8 @@ function LinkList(props) {
     return () => unsubscribe();
   }, [isTopPage, page]);
 
-  function getLinks() {      
+  function getLinks() {    
+    setLoading(true);  
     const hasCursor = Boolean(cursor);  
     const orderBy = { name: "created", direction: "desc"};
     
@@ -24,18 +27,34 @@ function LinkList(props) {
       orderBy.name = "voteCount";
     }
 
+    console.log(`at page = ${page} and has cursor of ${cursor?.url}`);
+
     var linkList = firebase.db.collection("links").orderBy(orderBy.name, orderBy.direction);
     
-    console.log(page);
-
-    if (page !== 1 && hasCursor) {
-      linkList = linkList.startAfter(cursor.created);
-    } 
-    
-    return linkList
+    if (page === 1) {
+      return linkList
       .limit(LINKS_PER_PAGE)
-      .onSnapshot(handleSnapshot);
-      
+      .onSnapshot(handleSnapshot);      
+    } else if (hasCursor) {
+      return linkList
+      .startAfter(cursor.created)
+      .limit(LINKS_PER_PAGE)
+      .onSnapshot(handleSnapshot);      
+    } 
+    else {
+      const offset = page * LINKS_PER_PAGE - LINKS_PER_PAGE;
+      axios.get(`https://us-central1-hooks-news-e53b4.cloudfunctions.net/linksPagination?offset=${offset}`)
+        .then(response => {
+          const links = response.data;
+          const lastLink = links[links.length - 1];
+          setLinks(links);
+          setCursor(lastLink);
+        });         
+        setLoading(false);
+        return () => {};
+    }
+
+    
   }
 
   function handleSnapshot(snapshot) {
@@ -46,6 +65,7 @@ function LinkList(props) {
     const lastLink = links[links.length - 1];
     setLinks(links);
     setCursor(lastLink);    
+    setLoading(false);
   }
 
   function visitPreviousPage() {
@@ -63,7 +83,7 @@ function LinkList(props) {
   const pageIndex = page ? (page - 1)*LINKS_PER_PAGE + 1 : 0;
 
   return (
-  <div>
+  <div style={{opacity: loading ? 0.25 : 1}}>
       {links.map((link, index) => (
         <LinkItem key={link.id} showCount={true} link={link} index={index + pageIndex} />
       ))}
